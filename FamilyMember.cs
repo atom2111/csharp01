20ba21ee5e7d706ccd940121d40ce9e02798a3c9
Atom <58501038+atom2111@users.noreply.github.com>
2024-04-15 23:15:51 +0500

    first commit

---
+++ FamilyMember.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace oop_lessons
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class FamilyMember
+    {
+        public FamilyMember Mother { get; set; }
+        public FamilyMember Father { get; set; }
+        public string Name { get; set; }
+        public Gender Sex { get; set; }
+        public List<FamilyMember> Siblings { get; } = new List<FamilyMember>();
+        public FamilyMember Spouse { get; set; } // Ссылка на супруга/супругу
+
+        public void ShowParents()
+        {
+            if (Mother != null)
+                Console.WriteLine($"Mother: {Mother.Name}");
+            if (Father != null)
+                Console.WriteLine($"Father: {Father.Name}");
+        }
+
+        public void ShowChildren()
+        {
+            Console.WriteLine($"Children of {Name}:");
+            if (this is AdultFamilyMember adult)
+            {
+                if (adult.Children != null)
+                {
+                    foreach (var child in adult.Children)
+                    {
+                        Console.WriteLine(child.Name);
+                    }
+                }
+            }
+        }
+
+        public void ShowSiblings()
+        {
+            Console.WriteLine($"Siblings of {Name}:");
+            foreach (var sibling in Siblings)
+            {
+                Console.WriteLine(sibling.Name);
+            }
+        }
+
+        public void ShowSpouse()
+        {
+            if (Spouse != null)
+            {
+                string relationship = Sex == Gender.Male ? "Wife" : "Husband";
+                Console.WriteLine($"{relationship}: {Spouse.Name}");
+            }
+        }
+
+        public void ShowGrandparents()
+        {
+            Console.WriteLine($"Grandparents of {Name}:");
+            if (Mother != null && Mother.Mother != null)
+                Console.WriteLine($"Maternal Grandmother: {Mother.Mother.Name}");
+            if (Mother != null && Mother.Father != null)
+                Console.WriteLine($"Maternal Grandfather: {Mother.Father.Name}");
+            if (Father != null && Father.Mother != null)
+                Console.WriteLine($"Paternal Grandmother: {Father.Mother.Name}");
+            if (Father != null && Father.Father != null)
+                Console.WriteLine($"Paternal Grandfather: {Father.Father.Name}");
+        }
+
+        // Метод для поиска самого старшего члена семьи
+        public FamilyMember FindOldestFamilyMember()
+        {
+            FamilyMember oldest = this;
+
+            if (Mother != null)
+            {
+                var motherOldest = Mother.FindOldestFamilyMember();
+                if (motherOldest != null && motherOldest.Name != oldest.Name)
+                {
+                    if (motherOldest.Name.CompareTo(oldest.Name) < 0)
+                    {
+                        oldest = motherOldest;
+                    }
+                }
+            }
+
+            if (Father != null)
+            {
+                var fatherOldest = Father.FindOldestFamilyMember();
+                if (fatherOldest != null && fatherOldest.Name != oldest.Name)
+                {
+                    if (fatherOldest.Name.CompareTo(oldest.Name) < 0)
+                    {
+                        oldest = fatherOldest;
+                    }
+                }
+            }
+
+            return oldest;
+        }
+
+        // Метод для вывода генеалогического дерева
+        public void ShowGenealogicalTree()
+        {
+            var oldest = FindOldestFamilyMember();
+            Console.WriteLine($"Genealogical Tree of {oldest.Name}:");
+            Console.WriteLine("-------------------------------------");
+            ShowGenealogicalTreeRecursive(oldest, 0);
+        }
+
+        // Рекурсивный метод для вывода генеалогического дерева
+        private void ShowGenealogicalTreeRecursive(FamilyMember member, int depth)
+        {
+            Console.Write(new string(' ', depth * 4));
+            if (depth > 0)
+            {
+                Console.Write(member.Mother != null ? "├──" : "└──");
+            }
+            Console.WriteLine(member.Name);
+
+            if (member.Mother != null)
+                ShowGenealogicalTreeRecursive(member.Mother, depth + 1);
+            if (member.Father != null)
+                ShowGenealogicalTreeRecursive(member.Father, depth + 1);
+        }
+    }
+
+    public class AdultFamilyMember : FamilyMember
+    {
+        public FamilyMember[] Children { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание членов семьи
+            FamilyMember grandmother1 = new FamilyMember { Name = "Grandma1", Sex = Gender.Female };
+            FamilyMember grandfather1 = new FamilyMember { Name = "Grandpa1", Sex = Gender.Male };
+            FamilyMember grandmother2 = new FamilyMember { Name = "Grandma2", Sex = Gender.Female };
+            FamilyMember grandfather2 = new FamilyMember { Name = "Grandpa2", Sex = Gender.Male };
+            FamilyMember mother = new FamilyMember { Name = "Mother", Sex = Gender.Female, Mother = grandmother1, Father = grandfather1 };
+            FamilyMember father = new FamilyMember { Name = "Father", Sex = Gender.Male, Mother = grandmother2, Father = grandfather2 };
+            AdultFamilyMember child1 = new AdultFamilyMember { Name = "Child1", Sex = Gender.Male, Mother = mother, Father = father };
+            AdultFamilyMember child2 = new AdultFamilyMember { Name = "Child2", Sex = Gender.Female, Mother = mother, Father = father };
+            FamilyMember spouse = new FamilyMember { Name = "Spouse", Sex = Gender.Male }; // Пример супруга
+
+            // Установка связей супругов
+            mother.Spouse = father;
+            father.Spouse = mother;
+
+            // Установка детей для взрослых членов семьи
+            mother.Siblings.Add(new FamilyMember { Name = "Aunt1", Sex = Gender.Female, Mother = grandmother1, Father = grandfather1 });
+            mother.Siblings.Add(new FamilyMember { Name = "Aunt2", Sex = Gender.Female, Mother = grandmother1, Father = grandfather1 });
+            father.Siblings.Add(new FamilyMember { Name = "Uncle1", Sex = Gender.Male, Mother = grandmother2, Father = grandfather2 });
+
+            // Вывод информации о родственниках
+            Console.WriteLine("Information about family members:");
+            Console.WriteLine("---------------------------------");
+            mother.ShowParents();
+            father.ShowParents();
+            child1.ShowChildren();
+            child2.ShowChildren();
+            child1.ShowSiblings();
+            child2.ShowSiblings();
+            mother.ShowSpouse(); // Вывод супруга/супруги
+            child1.ShowGrandparents();
+            child2.ShowGrandparents();
+
+            // Получение ссылки на самого старшего члена семьи и вывод его генеалогического дерева
+            FamilyMember oldest = mother.FindOldestFamilyMember();
+            oldest.ShowGenealogicalTree();
+
+            Console.ReadLine(); // Для предотвращения закрытия консоли сразу после вывода информации
+        }
+
+    }
+}
+
